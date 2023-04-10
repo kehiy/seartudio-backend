6f9d1995db3189fa548b48ffbae1c2a5dd4b76bf
kehiy <kehiiiiya@gmail.com>
2023-04-10 11:04:56 -0700

    add studio model

---
+++ src/models/studio.ts
@@ -0,0 +1,141 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+
+export class Studio extends Model {
+    id: string;
+    studioId: number;
+    name: string;
+    phoneNumber: number;
+    address: string;
+    province: string;
+    type: string;
+    pricePerHour: number;
+    openingHours: string;
+    logo: string;
+    image: string;
+    email: string;
+    telegramId: string;
+    isActive: boolean;
+    isPromoted: boolean;
+    isVeryfied: boolean;
+    expireDate: Date;
+    description: string;
+    passWord: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export default function (sequelize: Sequelize): typeof Studio {
+    Studio.init(
+        {
+            studioId: {
+                type: DataTypes.NUMBER,
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            phoneNumber: {
+                type: DataTypes.NUMBER,
+                allowNull: false,
+            },
+            address: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            province: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                values: [
+                    "آذربایجان شرقی",
+                    "آذربایجان غربی",
+                    "اردبیل",
+                    "اصفهان",
+                    "البرز",
+                    "ایلام",
+                    "بوشهر",
+                    "تهران",
+                    "چهارمحال و بختیاری",
+                    "خراسان جنوبی",
+                    "خراسان رضوی",
+                    "خراسان شمالی",
+                    "خوزستان",
+                    "زنجان",
+                    "سمنان",
+                    "سیستان و بلوچستان",
+                    "فارس",
+                    "قزوین",
+                    "قم",
+                    "کردستان",
+                    "کرمان",
+                    "کرمانشاه",
+                    "کهگیلویه و بویراحمد",
+                    "گلستان",
+                    "گیلان",
+                    "لرستان",
+                    "مازندران",
+                    "مرکزی",
+                    "هرمزگان",
+                    "همدان",
+                    "یزد"
+                ],
+            },
+            type: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                values: ["خانگی", "حرفه ای"],
+            },
+            pricePerHour: {
+                type: DataTypes.NUMBER,
+                allowNull: false,
+            },
+            openingHours: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            logo: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            image: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            telegramId: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            isActive: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+            },
+            isPromoted: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+            },
+            isVeryfied: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+            },
+            expireDate: {
+                type: DataTypes.DATE,
+                allowNull: true,
+            },
+            description: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            passWord: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            }
+        },
+        { sequelize }
+    );
+
+    return Studio;
+}
