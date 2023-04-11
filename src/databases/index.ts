c3ad00d6f1a8cfdd2715a2270a24734afcdf0404
kehiy <kehiiiiya@gmail.com>
2023-04-11 14:19:19 -0700

    add Newsletter to db index.ts

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -2,6 +2,7 @@ import Sequelize from 'sequelize';
 import { NODE_ENV, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } from '../config';
 import Admin from '../models/admin';
 import Studio from '../models/studio';
+import Newsletter from '../models/newsLetter';
 
 const sequelize = new Sequelize.Sequelize(DB_NAME, DB_USER, DB_PASS, {
   dialect: 'postgres',
@@ -18,6 +19,7 @@ sequelize.authenticate();
 const DB = {
   Admin: Admin(sequelize),
   Studio: Studio(sequelize),
+  Newsletter: Newsletter(sequelize),
   sequelize,
 };
 
