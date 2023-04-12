638df7f67cf4a3ae8ef68f7bbc2a1973304d9a4c
kehiy <kehiiiiya@gmail.com>
2023-04-12 07:55:53 -0700

    ff

--- src/controllers/newsLetterController.ts
+++ src/controllers/newsLetterController.ts
@@ -1,12 +1,11 @@
 import { apiResponse } from 'utils/apiRespones';
 import DB from '../databases';
-import { Request, Response } from 'express';
 import messageEnum from 'enums/messageEnum';
 
 const Newsletter = DB.Newsletter;
 
-export const addNewsLetter = async (req : Request, res:Response) => {
-  const {email} = req.body;
-  await Newsletter.create({email});
-  return apiResponse(res,201,messageEnum.created_201,{email});
+export const addNewsLetter = async (req, res) => {
+  const { email } = req.body;
+  const newData = await Newsletter.create({ email });
+  return apiResponse(res, 201, messageEnum.created_201, { newData });
 };
