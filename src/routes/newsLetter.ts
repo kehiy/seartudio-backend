6c4f0e7ca63901992b0e5d8ba4b16b1b4e2ea7ef
kehiy <kehiiiiya@gmail.com>
2023-04-12 07:48:17 -0700

    add handler for newsletter router

--- src/routes/newsLetter.ts
+++ src/routes/newsLetter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { addNewsLetter } from '../controllers/newsLetterController';
+import { handlerAsync } from 'utils/handler';
 
 const router = Router();
 
 
-router.post('/', addNewsLetter);
+router.post('/', handlerAsync(addNewsLetter));
 
 
 export default router;
