6a550cc9728a8655e1c7ec4fb7c856272ffce99c
kehiy <kehiiiiya@gmail.com>
2023-04-12 09:01:43 -0700

    add validator to router.

--- src/routes/newsLetter.ts
+++ src/routes/newsLetter.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { addNewsLetter } from '../controllers/newsLetterController';
 import { handlerAsync } from 'utils/handler';
-
+import { newsLetterValidate, emailValidate } from 'validators/newsLetterValidator';
 const router = Router();
 
 
-router.post('/',handlerAsync(addNewsLetter));
+router.post('/', emailValidate(), newsLetterValidate, handlerAsync(addNewsLetter));
 
 
 export default router;
