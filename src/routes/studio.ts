e434ba0c0caf89fb8bc73d9f48d7be5dec3c5a36
kehiy <kehiiiiya@gmail.com>
2023-04-15 13:29:00 -0700

    all user test done

--- src/routes/studio.ts
+++ src/routes/studio.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { addStudio, studioSignup, updateStudio, updateImage } from '../controllers/studioController';
+import { addStudio, studioSignup, updateStudio, updateImage, updateLogo } from '../controllers/studioController';
 import { handlerAsync } from 'utils/handler';
 import { studioValidate, studioValidateErr, signupValidate, updateValidate } from '../validators/newStudioValidator';
 import { authenticated } from 'middlewares/auth';
@@ -11,6 +11,7 @@ router.post('/add', studioValidate(), studioValidateErr, handlerAsync(addStudio)
 router.post('/signup', signupValidate(), studioValidateErr, handlerAsync(studioSignup));
 router.post('/update', authenticated, updateValidate(), studioValidateErr, handlerAsync(updateStudio));
 router.post('/updateImage', authenticated, handlerAsync(updateImage));
+router.post('/updateLogo', authenticated, handlerAsync(updateLogo));
 
 
 export default router;
