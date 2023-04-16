fe977b1c4da06dad0e4f60cc132900f99e447cf5
kehiy <kehiiiiya@gmail.com>
2023-04-16 01:45:38 -0700

    get active studios done

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,8 +1,18 @@
 import messageEnum from "enums/messageEnum";
 import { apiResponse } from "utils/apiRespones";
 import DB from "../databases";
+import Dto from "dto/studioDto";
 
 const Admin = DB.Admin;
 const Studio = DB.Studio;
 
 
+export const getActiveStudios = async (req,res) => {
+    const activeStudios = await Studio.findAll({
+        where:{
+            isActive:true
+        }
+    });
+
+    return apiResponse(res,200,messageEnum.get_success,new Dto(activeStudios));
+}
