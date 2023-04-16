8e5667b9187a6c41b1159e1487fd35701b4b46e2
kehiy <kehiiiiya@gmail.com>
2023-04-16 07:31:38 -0700

    convert updateadmin studio to (activate studio, promot studio and verifystudio)

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -7,27 +7,37 @@ const Admin = DB.Admin;
 const Studio = DB.Studio;
 
 
-export const getActiveStudios = async (req,res) => {
+export const getActiveStudios = async (req, res) => {
     const activeStudios = await Studio.findAll({
-        where:{
-            isActive:true
+        where: {
+            isActive: true
         }
     });
 
-    return apiResponse(res,200,messageEnum.get_success,new Dto(activeStudios));
+    return apiResponse(res, 200, messageEnum.get_success, new Dto(activeStudios));
 }
 
-export const getDeactiveStudios = async (req,res) => {
+export const getDeactiveStudios = async (req, res) => {
     const activeStudios = await Studio.findAll({
-        where:{
-            isActive:false
+        where: {
+            isActive: false
         }
     });
 
-    return apiResponse(res,200,messageEnum.get_success,new Dto(activeStudios));
+    return apiResponse(res, 200, messageEnum.get_success, new Dto(activeStudios));
 }
 
-export const getAllStudios = async (req,res) => {
+export const getAllStudios = async (req, res) => {
     const activeStudios = await Studio.findAll();
-    return apiResponse(res,200,messageEnum.get_success,new Dto(activeStudios));
+    return apiResponse(res, 200, messageEnum.get_success, new Dto(activeStudios));
+}
+
+export const activateStudio = async (req, res) => {
+    const { subscriptionMonths, studioId } = req.body;
+
+    let endDate = new Date(); // Get the current date/time
+
+    endDate.setMonth(endDate.getMonth() + subscriptionMonths); // Add the number of months to the current date
+
+    
 }
