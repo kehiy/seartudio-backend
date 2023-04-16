58b685b06a341960e6256d73ae5943919e4fce33
kehiy <kehiiiiya@gmail.com>
2023-04-16 13:04:56 -0700

    cron job done

---
+++ src/cron/expierDate.ts
@@ -0,0 +1,51 @@
+import messageEnum from "enums/messageEnum";
+import { apiResponse } from "utils/apiRespones";
+import { Telegraf, Markup } from "telegraf";
+import DB from "../databases";
+import { Op } from "sequelize";
+
+const Studio = DB.Studio;
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
+const cornKey = process.env.CRONJOB_KEY;
+
+
+const checkExpier = async (req, res) => {
+    const reqCornKey = req.body.cornKey;
+    const now = new Date();
+
+
+    if (reqCornKey !== cornKey) {
+        return apiResponse(res, 401, messageEnum.err_forbidden, {});
+    }
+
+    const bot = new Telegraf(botToken);
+    const keyboardMarkup = Markup.inlineKeyboard([
+        { text: "ارتباط با پشتیبانی", url: "https://t.me/seartudio_support" }
+    ]);
+
+    const recordsToDeactivate = await Studio.update(
+        {isActive: false},
+        {where: {
+            isActive: true,
+            expireDate: { [Op.lte]: now }
+        },
+        returning: true
+    }).catch(err=>{
+        throw err;
+    });
+
+    recordsToDeactivate[1].forEach(async studio=>{
+        bot.telegram.sendMessage(studio.telegramId, messageEnum.telegramStudioExpire.message.replace("نام",studio.name), keyboardMarkup);
+        await delay(1000);
+    });
+
+    bot.launch();
+    return apiResponse(res,200,messageEnum.cronDone,recordsToDeactivate[1]);
+}
+
+
+
+
+function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- src/enums/messageEnum.ts
+++ src/enums/messageEnum.ts
@@ -1,42 +1,55 @@
 export default {
-    created_201: {
-      code: 'created_201',
-      text: 'created',
-    },
-    bad_request:{
-      code:'bad_request',
-      text:'bad request'
-    },
-    studio_exist:{
-      code:'studio_exist',
-      text:'studio is exist'
-    },
-    err_Unsupported_Media_Type:{
-      code:"err_Unsupported_Media_Type",
-      text:"just png/jpg/jpeg media types supported."
-    },
-    login_success:{
-      code:"login_success",
-      text:"loged in successfully"
-    },
-    login_faild:{
-      code:"login_faild",
-      text:"email or pass word is invalid!"
-    },
-    err_unauthorized:{
-      code:"err_unauthorized",
-      text:"Unauthorized"
-    },
-    err_forbidden:{
-      code:"err_forbidden",
-      text:"Forbidden"
-    },
-    get_success:{
-      code:"get_success",
-      text:"ok"
-    },
-    server_error:{
-      code:"server_error",
-      text:"somethong went worng!"
-    },
+  created_201: {
+    code: 'created_201',
+    text: 'created',
+  },
+  bad_request: {
+    code: 'bad_request',
+    text: 'bad request'
+  },
+  studio_exist: {
+    code: 'studio_exist',
+    text: 'studio is exist'
+  },
+  err_Unsupported_Media_Type: {
+    code: "err_Unsupported_Media_Type",
+    text: "just png/jpg/jpeg media types supported."
+  },
+  login_success: {
+    code: "login_success",
+    text: "loged in successfully"
+  },
+  login_faild: {
+    code: "login_faild",
+    text: "email or pass word is invalid!"
+  },
+  err_unauthorized: {
+    code: "err_unauthorized",
+    text: "Unauthorized"
+  },
+  err_forbidden: {
+    code: "err_forbidden",
+    text: "Forbidden"
+  },
+  get_success: {
+    code: "get_success",
+    text: "ok"
+  },
+  server_error: {
+    code: "server_error",
+    text: "somethong went worng!"
+  },
+  cronDone: {
+    code: "cronDone",
+    text: "cron job was done successfully."
+  },
+  telegramStudioExpire: {
+    message: `
+                            کابر گرامی
+            مالک استودیو نام
+            مدت زمان اشتراک سرتودیو شما به پایان رسیده است
+            لطفا جهت تمدید اشتراک خود با  پشتیبانی سرتودیو تماس بگیرید.👀❤️
+            `
+
+  }
 };
