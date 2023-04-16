34d2893a7b6f3849a320fabc1a75b5f4d205e1b9
kehiy <kehiiiiya@gmail.com>
2023-04-16 01:42:36 -0700

    init admin controller

---
+++ src/controllers/adminController.ts
@@ -0,0 +1,8 @@
+import messageEnum from "enums/messageEnum";
+import { apiResponse } from "utils/apiRespones";
+import DB from "../databases";
+
+const Admin = DB.Admin;
+const Studio = DB.Studio;
+
+
