7513c7f3af866c58a8ba30a9bcf17cd33320460c
kehiy <kehiiiiya@gmail.com>
2023-05-08 08:20:11 -0700

    sefef

--- src/routes/studio.ts
+++ src/routes/studio.ts
@@ -7,11 +7,11 @@ import { authenticated } from 'middlewares/auth';
 const router = Router();
 
 
+router.post('/updateLogo', authenticated, handlerAsync(updateLogo));
+router.post('/updateImage', authenticated, handlerAsync(updateImage));
+router.post('/update', authenticated, updateValidate(), studioValidateErr, handlerAsync(updateStudio));
 router.post('/add', studioValidate(), studioValidateErr, handlerAsync(addStudio));
 router.post('/signup', signupValidate(), studioValidateErr, handlerAsync(studioSignup));
-router.post('/updateImage', authenticated, handlerAsync(updateImage));
-router.post('/updateLogo', authenticated, handlerAsync(updateLogo));
-router.post('/update', authenticated, updateValidate(), studioValidateErr, handlerAsync(updateStudio));
 router.get('/id/:studioId', handlerAsync(getStudioDetail));
 router.get('/getMe', handlerAsync(getMe));
 router.get('/', getAllStudios);
